427c4be9f9197776ffa748b4c17ad0ac3c81ed65
Jason <extremewinds@gmail.com>
2024-03-02 09:30:06 +0100

    add switch case example

--- src/control-flow.js
+++ src/control-flow.js
@@ -4,7 +4,7 @@ export const Greetings = {
   GOOD_EVENING: "Good evening",
 };
 
-export function howGreetAtTime(hour) {
+export function howGreetAtTime_elseIfExample(hour) {
   if (hour >= 6 && hour < 12) {
     return Greetings.GOOD_MORNING;
   } else if (hour >= 12 && hour < 18) {
@@ -12,10 +12,25 @@ export function howGreetAtTime(hour) {
   } else return Greetings.GOOD_EVENING;
 }
 
-export function howGreetAtTime2(hour) {
-  if (hour >= 6 && hour < 12) {
-    return "Good morning";
-  } else if (hour >= 12 && hour < 18) {
-    return "Good afternoon";
-  } else return "Good evening";
+export function howGreetAtTime_switchCaseExample(hour) {
+  switch (hour) {
+    case 6:
+    case 7:
+    case 8:
+    case 9:
+    case 10:
+    case 11:
+      return Greetings.GOOD_MORNING;
+      break;
+    case 12:
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+      return Greetings.GOOD_AFTERNOON;
+      break;
+    default:
+      return Greetings.GOOD_EVENING;
+  }
 }
--- tests/control-flow.test.js
+++ tests/control-flow.test.js
@@ -1,18 +1,19 @@
 import { describe, test, it, expect } from "vitest";
-import { howGreetAtTime, howGreetAtTime2 } from "../src/control-flow";
+import {
+  howGreetAtTime_elseIfExample,
+  howGreetAtTime_switchCaseExample,
+} from "../src/control-flow";
 
 describe.each([
   { hour: 10, expectedGreeting: "Good morning" },
   { hour: 15, expectedGreeting: "Good afternoon" },
   { hour: 20, expectedGreeting: "Good evening" },
-])
-(`At $hour we should say $expectedGreeting`, ({ hour, expectedGreeting }) => {
-
+])(`At $hour we should say $expectedGreeting`, ({ hour, expectedGreeting }) => {
   test(`so we said ${expectedGreeting}`, () => {
-    expect(howGreetAtTime(hour)).toBe(expectedGreeting);
+    expect(howGreetAtTime_elseIfExample(hour)).toBe(expectedGreeting);
   });
 
   test(`so we said2 ${expectedGreeting}`, () => {
-    expect(howGreetAtTime2(hour)).toBe(expectedGreeting);
+    expect(howGreetAtTime_switchCaseExample(hour)).toBe(expectedGreeting);
   });
 });
