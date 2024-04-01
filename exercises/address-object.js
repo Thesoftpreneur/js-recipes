a9bd917e973c04fea7f46b4b2f76f3d749f8bcf7
Jason <extremewinds@gmail.com>
2024-04-01 13:26:34 +0200

    add constructor function

--- exercises/address-object.js
+++ exercises/address-object.js
@@ -1,15 +1,31 @@
 import { faker } from "@faker-js/faker";
 
-let myAddresss = {
-  street: faker.location.street(),
-  city: faker.location.city(),
-  zipCode: faker.location.zipCode(),
-};
+// let myAddresss = {
+//   street: faker.location.street(),
+//   city: faker.location.city(),
+//   zipCode: faker.location.zipCode(),
+// };
 
-function showAddress(address) {
-  for (let key in address) {
-    console.log(key, address[key]);
-  }
+// function showAddress(address) {
+//   for (let key in address) {
+//     console.log(key, address[key]);
+//   }
+// }
+
+// showAddress(myAddresss);
+
+function Address() {
+    this.street = faker.location.street();
+    this.city = faker.location.city();
+    this.zipCode = faker.location.zipCode();
+    this.showAddress = function() {
+        for(let key in this) {
+            if (typeof this[key] !== "function") {
+                console.log(key, this[key]);
+            }
+        }
+    }
 }
 
-showAddress(myAddresss);
+const address = new Address();
+address.showAddress();
